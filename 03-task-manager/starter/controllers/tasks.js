ef5d16e9f421b26c160ff15dc09e48ebea9eb01e
akabishau <aleksey.kabishau@icloud.com>
2023-04-04 15:46:29 -0400

    complete update and delete methods logic

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -7,7 +7,7 @@ const getAllTasks = async (req, res) => {
     } catch (err) {
         res.status(500).json({ msg: err })
     }
-    
+
 }
 
 const createTask = async (req, res) => {
@@ -17,8 +17,8 @@ const createTask = async (req, res) => {
     } catch (err) {
         res.status(500).json({ msg: err })
     }
-    
-    
+
+
 }
 
 const getTask = async (req, res) => {
@@ -35,12 +35,36 @@ const getTask = async (req, res) => {
     }
 }
 
-const updateTask = (req, res) => {
-    res.send('update task')
+const updateTask = async (req, res) => {
+    try {
+        const id = req.params.id
+        // params: id, data, options (by default it returns the old data)
+        const task = await Task.findOneAndUpdate(
+            { _id: id },
+            req.body,
+            { new: true, runValidators: true }
+        )
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id : ${id}` })
+        }
+
+        res.status(200).json({ task })
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
-const deleteTask = (req, res) => {
-    res.send('remove task')
+const deleteTask = async (req, res) => {
+    try {
+        const id = req.params.id
+        const task = await Task.findOneAndDelete({ _id: id })
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id : ${id}` })
+        }
+        res.status(200).json({ task })
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
 
