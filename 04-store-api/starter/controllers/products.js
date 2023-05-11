2b29bd998a0d4cef7f164cf067e1ea3bb41dd0d1
akabishau <aleksey.kabishau@icloud.com>
2023-05-11 15:09:16 -0400

    add default and user provided pagination to the output results

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
     //const products = await Product.find( { featured: true, company: 'ikea' })
 
     // using regex as search
-    const products = await Product.find({}).sort('-price').select('name price')
+    const products = await Product.find({}).sort('-price').select('name price').skip(1).limit(5)
     res.status(200).json({ total: products.length, items: products })
 }
 
@@ -48,8 +48,14 @@ const getAllProducts = async (req, res) => {
         result.select(fieldsList)
     }
 
-    const products = await result
+    // pagination using limit and skip
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+    result = result.skip(skip).limit(limit)
 
+    
+    const products = await result
     res.status(200).json({ total: products.length, items: products })
 }
 
